ad75ca0a840fd21394fc6c5e6568b056f4befcb2
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2023-02-03 18:58:52 -0300

    cosa rara

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -200,7 +200,7 @@ function tieneTresDigitos(numero){
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
-  //Retornar el valor final.
+  //Retornar el valor Final.
   //Usar el bucle do ... while.
   var a = numero;
   var i = 0;
