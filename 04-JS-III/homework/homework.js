4aa9e08c2a1efeade1ee397a1fbf89193680184b
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2023-02-15 13:08:33 -0300

    me partio la cabeza

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -118,14 +118,12 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  if(arguments.length > 1){
-    return 0
-  }
-  let total = 1;
-  for(let i = 0; i < arguments.length; i++){
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
     total = total * arguments[i];
   }
-  return total
+  return total;
 }
 
 
@@ -147,8 +145,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  if(numeroDeDia === 1 || numeroDeDia === 7) return "Es fin de semana";{
-  return "Es dia Laboral";
+  //Domingo 1, Lunes 2 ; martes 3; miercoles4, jueves 5, viernes 6, sabado 7, 
+  if(numeroDeDia === 1 || numeroDeDia === 7){
+     return "Es fin de semana";}
+  else {
+        return "Es dia Laboral";
   }
 } 
 
@@ -158,10 +159,11 @@ function empiezaConNueve(n) {
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
 
-  let numero = n.toString()
-  if (numero.charAt[0] === "9"){
-    return true;
-  } return false; 
+  let num = n.toString()
+  if(num.charAt(0) === "9"){
+    return true
+  }
+  return false
 }
 
 
@@ -218,22 +220,26 @@ function breakStatement(numero) {
   //Guardar cada nuevo valor en un array. 
   //Devolver el array
   //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
-  //devolver:  var nuevoArray = [];
+  //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  var nuevoArray = [];
-  var suma = numero
-  for (var i = 0;i < 10; i++){
-      suma = suma + 2;{
-        if(suma ===i) break;
-        nuevoArray.push(suma)
-      }
-
-    
-  
-
+  var array = [];
+  var suma = numero;
+  for(var i= 0; i<10; i++) {
+    suma = suma + 2;
+    if(suma === i) break;
+    else {
+      array.push(suma);
+    }
+  }
+  if(i < 10) {
+    return 'Se interrumpió la ejecución';
+  }
+  else {
+      return array;
+  }
 }
-}
+
 
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
@@ -244,14 +250,13 @@ function continueStatement(numero) {
   // Tu código:
  nuevoArray = [];
  var suma = numero;
-  for (var i = 0; i < 5; i++){
-    suma = suma + 2 ;
-    if(suma = i ) brack;
-  else { nuevoArray.push(suma)}
-  }
-}
-
-
+  for (var i = 0; i < 10; i++){
+      if(i===5) continue;
+      else{suma = suma + 2;
+        nuevoArray.push ( suma )
+      }
+    }return nuevoArray;
+}  
 // No modificar nada debajo de esta línea
 // --------------------------------
 
